c0d15f522a224db935275870132b0a288fb4a105
vantoan1511 <vantoan1517@gmail.com>
2024-01-13 21:07:20 +0700

    update handle login

--- src/main/java/com/newswebsite/main/controller/web/LoginController.java
+++ src/main/java/com/newswebsite/main/controller/web/LoginController.java
@@ -1,17 +1,25 @@
 package com.newswebsite.main.controller.web;
 
+import com.newswebsite.main.utils.FlashMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
     @GetMapping
-    public ModelAndView getLoginPage() {
+    public ModelAndView getLoginPage(@RequestParam(value = "invalidCredentials", required = false) boolean invalidCredentials,
+                                     RedirectAttributes attributes) {
         String viewName = "web/login";
+        if (invalidCredentials) {
+            attributes.addFlashAttribute("message", FlashMessage.danger("Thông tin đăng nhập không hợp lệ"));
+            viewName = "redirect:/login";
+        }
         ModelAndView view = new ModelAndView(viewName);
         return view;
     }
--- src/main/java/com/newswebsite/main/entity/User.java
+++ src/main/java/com/newswebsite/main/entity/User.java
@@ -1,21 +1,52 @@
 package com.newswebsite.main.entity;
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 
 @Entity
 @Table(name = "user")
 @Data
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column
     private String email;
+    @Column
     private String username;
+    @Column
     private String password;
+    @Column(name = "avatar_url")
     private String avatarUrl;
+    @Column
     private String token;
-    private boolean enabled;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
