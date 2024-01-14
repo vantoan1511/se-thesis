c1d7f65fe5cf7e8f2c5e4fe9c939f8f312d0d87b
vantoan1511 <vantoan1517@gmail.com>
2024-01-14 22:21:19 +0700

    optimize code

--- src/main/java/com/newswebsite/main/service/impl/CustomUserDetailsService.java
+++ src/main/java/com/newswebsite/main/service/impl/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.newswebsite.main.service.impl;
 import com.newswebsite.main.dto.UserDTO;
 import com.newswebsite.main.entity.User;
 import com.newswebsite.main.repository.UserRepo;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,16 +16,13 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepo userRepo;
 
+    private final ModelMapper mapper = new ModelMapper();
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = isEmail(username) ? userRepo.findByEmail(username) : userRepo.findByUsername(username);
         if (user == null) throw new UsernameNotFoundException("Không tìm thấy người dùng với thông tin " + username);
-        return UserDTO.builder()
-                .email(user.getEmail())
-                .username(user.getUsername())
-                .password(user.getPassword())
-                .enabled(true)
-                .build();
+        return mapper.map(user, UserDTO.class);
     }
 
     private boolean isEmail(String text) {
