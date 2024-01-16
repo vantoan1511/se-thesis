829b0bd8e670fe162c9d0a71d9943c37558eb926
vantoan1511 <vantoan1517@gmail.com>
2024-01-16 15:41:42 +0700

    small update

--- src/main/java/com/newswebsite/main/service/impl/EmailService.java
+++ src/main/java/com/newswebsite/main/service/impl/EmailService.java
@@ -6,10 +6,15 @@ import org.simplejavamail.api.mailer.Mailer;
 import org.simplejavamail.api.mailer.config.TransportStrategy;
 import org.simplejavamail.email.EmailBuilder;
 import org.simplejavamail.mailer.MailerBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService implements IEmailService {
+
+    @Value("${email.password}")
+    private String PW;
+
     @Override
     public void sendSimpleEmail(String to, String subject, String text) {
         Email email = EmailBuilder.startingBlank()
@@ -18,7 +23,7 @@ public class EmailService implements IEmailService {
                 .withSubject(subject)
                 .withHTMLText(text)
                 .buildEmail();
-        Mailer mailer = MailerBuilder.withSMTPServer("smtp.gmail.com", 587, "vantoan1517@gmail.com", "kjjmdtdufyrgfsqz")
+        Mailer mailer = MailerBuilder.withSMTPServer("smtp.gmail.com", 587, "vantoan1517@gmail.com", PW)
                 .withTransportStrategy(TransportStrategy.SMTP_TLS)
                 .buildMailer();
         mailer.sendMail(email);
