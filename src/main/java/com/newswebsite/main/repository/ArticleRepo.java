46814cce279dc3d5a034c5577a6b495f57e4cf3a
vantoan1511 <vantoan1517@gmail.com>
2024-01-19 18:09:30 +0700

    add find by alias

--- src/main/java/com/newswebsite/main/repository/ArticleRepo.java
+++ src/main/java/com/newswebsite/main/repository/ArticleRepo.java
@@ -6,5 +6,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepo extends JpaRepository<Article, Long> {
+
+    Article findByAlias(String alias);
+
     Page<Article> findAll(Pageable pageable);
+
 }
