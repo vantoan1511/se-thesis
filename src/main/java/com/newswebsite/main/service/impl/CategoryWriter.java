77d98f6fdaa31c48905abcd1dd9553c1c80e0418
vantoan1511 <vantoan1517@gmail.com>
2024-01-23 10:01:53 +0700

    update save method

--- src/main/java/com/newswebsite/main/service/impl/CategoryWriter.java
+++ src/main/java/com/newswebsite/main/service/impl/CategoryWriter.java
@@ -2,11 +2,16 @@ package com.newswebsite.main.service.impl;
 
 import com.newswebsite.main.dto.CategoryDTO;
 import com.newswebsite.main.entity.Category;
+import com.newswebsite.main.exception.CategoryCodeNotFoundException;
 import com.newswebsite.main.mapper.CollectionMapper;
 import com.newswebsite.main.repository.CategoryRepo;
 import com.newswebsite.main.service.ICategoryWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class CategoryWriter implements ICategoryWriter {
@@ -17,8 +22,34 @@ public class CategoryWriter implements ICategoryWriter {
     private final CollectionMapper mapper = new CollectionMapper();
 
     @Override
+    @Transactional
     public CategoryDTO save(CategoryDTO categoryDTO) {
         Category category = mapper.map(categoryDTO, Category.class);
-        return mapper.map(categoryRepo.save(category), CategoryDTO.class);
+        category.setPublishedAt(new Date());
+        if (categoryDTO.getId() != null) {
+            Category oldCategory = categoryRepo.findOne(categoryDTO.getId());
+            category.setCreatedAt(oldCategory.getCreatedAt());
+            category.setCreatedBy(oldCategory.getCreatedBy());
+            category.setPublishedAt(oldCategory.getPublishedAt());
+        }
+        category = categoryRepo.save(category);
+        return mapper.map(category, CategoryDTO.class);
+    }
+
+    @Override
+    @Transactional
+    public void delete(long id) {
+        if (!categoryRepo.exists(id)) {
+            throw new CategoryCodeNotFoundException("Chuyên mục không tồn tại " + id);
+        }
+        categoryRepo.delete(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteMultiple(List<Long> ids) {
+        for (long id : ids) {
+            delete(id);
+        }
     }
 }
