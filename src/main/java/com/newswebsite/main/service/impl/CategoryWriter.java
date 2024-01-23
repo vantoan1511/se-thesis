acbe765ddaf2dd8424ed82be2a50dec70414a06f
vantoan1511 <vantoan1517@gmail.com>
2024-01-23 12:18:28 +0700

    update to add parent

--- src/main/java/com/newswebsite/main/service/impl/CategoryWriter.java
+++ src/main/java/com/newswebsite/main/service/impl/CategoryWriter.java
@@ -6,8 +6,10 @@ import com.newswebsite.main.exception.CategoryCodeNotFoundException;
 import com.newswebsite.main.mapper.CollectionMapper;
 import com.newswebsite.main.repository.CategoryRepo;
 import com.newswebsite.main.service.ICategoryWriter;
+import com.newswebsite.main.utils.SlugGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.transaction.Transactional;
 import java.util.Date;
@@ -25,13 +27,27 @@ public class CategoryWriter implements ICategoryWriter {
     @Transactional
     public CategoryDTO save(CategoryDTO categoryDTO) {
         Category category = mapper.map(categoryDTO, Category.class);
-        category.setPublishedAt(new Date());
-        if (categoryDTO.getId() != null) {
-            Category oldCategory = categoryRepo.findOne(categoryDTO.getId());
+        Category oldCategory = categoryDTO.getId() != null ? categoryRepo.findOne(categoryDTO.getId()) : new Category();
+        Category parent = categoryDTO.getParentAlias().isBlank() ? null : categoryRepo.findByAlias(categoryDTO.getParentAlias());
+
+        if (oldCategory != null) {
             category.setCreatedAt(oldCategory.getCreatedAt());
             category.setCreatedBy(oldCategory.getCreatedBy());
             category.setPublishedAt(oldCategory.getPublishedAt());
         }
+
+        String newAlias = StringUtils.hasText(category.getAlias()) ? category.getAlias() : category.getTitle();
+        String oldAlias = oldCategory.getAlias();
+        if (!newAlias.equals(oldAlias) && !isUniqueAlias(newAlias)) {
+            newAlias = SlugGenerator.generateUniqueSlug(newAlias);
+        } else {
+            newAlias = SlugGenerator.slugify.slugify(newAlias);
+        }
+        
+        category.setAlias(newAlias);
+        category.setPublishedAt(new Date());
+        category.setParent(parent);
+
         category = categoryRepo.save(category);
         return mapper.map(category, CategoryDTO.class);
     }
@@ -51,5 +67,9 @@ public class CategoryWriter implements ICategoryWriter {
         for (long id : ids) {
             delete(id);
         }
+    }
+
+    private boolean isUniqueAlias(String alias) {
+        return categoryRepo.findByAlias(alias) == null;
     }
 }
