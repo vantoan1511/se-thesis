cb35bfa2981063aa280315a2d0b32858e23e5b0d
vantoan1511 <vantoan1517@gmail.com>
2024-01-23 10:03:00 +0700

    add delete and deleteMultiple method

--- src/main/java/com/newswebsite/main/service/ICategoryWriter.java
+++ src/main/java/com/newswebsite/main/service/ICategoryWriter.java
@@ -2,6 +2,12 @@ package com.newswebsite.main.service;
 
 import com.newswebsite.main.dto.CategoryDTO;
 
+import java.util.List;
+
 public interface ICategoryWriter {
     CategoryDTO save(CategoryDTO categoryDTO);
+
+    void delete(long id);
+
+    void deleteMultiple(List<Long> ids);
 }
