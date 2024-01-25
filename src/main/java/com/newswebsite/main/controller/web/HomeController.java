ece49997b54ecc65a1c829c443c36aa8abe98609
vantoan1511 <vantoan1517@gmail.com>
2024-01-25 18:41:51 +0700

    Update code - Put featured articles - Put latest articles

--- src/main/java/com/newswebsite/main/controller/web/HomeController.java
+++ src/main/java/com/newswebsite/main/controller/web/HomeController.java
@@ -2,6 +2,7 @@ package com.newswebsite.main.controller.web;
 
 import com.newswebsite.main.service.articleservice.IArticleReader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,14 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomeController {
 
     @Autowired
-    private IArticleReader articleRetrievalService;
+    private IArticleReader articleReader;
 
     @GetMapping
     public ModelAndView getHomePage() {
         String viewName = "web/home";
         ModelAndView view = new ModelAndView(viewName);
+        view.addObject("featured", articleReader.getFeaturedArticles(new PageRequest(0, 3)));
+        view.addObject("latest", articleReader.getLatestArticles(new PageRequest(0, 10)));
         return view;
     }
 }
