595192b4854b3069d459e81e13d4d90c2fe4bca0
vantoan1511 <vantoan1517@gmail.com>
2024-01-31 15:14:27 +0700

    Update Controller Advice to return custom error page

--- src/main/java/com/newswebsite/main/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/com/newswebsite/main/controller/advice/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.newswebsite.main.controller.advice;
 
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,9 +11,15 @@ import javax.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final String DEFAULT_ADMIN_ERROR_VIEW = "admin/404";
+    private final String DEFAULT_WEB_ERROR_VIEW = "web/404";
+
     @ExceptionHandler(value = Exception.class)
-    public ModelAndView defaultErrorHandler(Exception ex, HttpServletRequest req) {
-        String viewName = isAdminRequest(req) ? "admin/404" : "web/404";
+    public ModelAndView defaultErrorHandler(Exception ex, HttpServletRequest req) throws Exception {
+        if (AnnotationUtils.findAnnotation(ex.getClass(), ResponseStatus.class) != null) {
+            throw ex;
+        }
+        String viewName = isAdminRequest(req) ? DEFAULT_ADMIN_ERROR_VIEW : DEFAULT_WEB_ERROR_VIEW;
         return new ModelAndView(viewName);
     }
 
