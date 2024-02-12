bee30efec930b5571469b6aeffdb2b0bd391f01e
vantoan1511 <vantoan1517@gmail.com>
2024-02-12 21:08:07 +0700

    Update base url

--- src/main/java/com/newswebsite/main/constant/Application.java
+++ src/main/java/com/newswebsite/main/constant/Application.java
@@ -1,5 +1,5 @@
 package com.newswebsite.main.constant;
 
 public class Application {
-    public static final String BASE_URL = "http://localhost:8080";
+    public static final String BASE_URL = "http://localhost";
 }
