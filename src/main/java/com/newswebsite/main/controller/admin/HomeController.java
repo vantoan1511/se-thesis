632c9a80b5813db12526496ee5ee73d21cc51551
vantoan1511 <vantoan1517@gmail.com>
2024-02-12 21:07:48 +0700

    Update to handle both /admin/home and /admin/

--- src/main/java/com/newswebsite/main/controller/admin/HomeController.java
+++ src/main/java/com/newswebsite/main/controller/admin/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping({"/home", "/"})
     public String getHomePage() {
         return "admin/home";
     }
