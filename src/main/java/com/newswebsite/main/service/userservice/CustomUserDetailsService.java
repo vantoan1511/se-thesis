2c7b59f44155266c06ade7bfd1214b9284744fac
vantoan1511 <vantoan1517@gmail.com>
2024-02-25 19:01:37 +0700

    Optimize injection

--- src/main/java/com/newswebsite/main/service/userservice/CustomUserDetailsService.java
+++ src/main/java/com/newswebsite/main/service/userservice/CustomUserDetailsService.java
@@ -1,11 +1,10 @@
 package com.newswebsite.main.service.userservice;
 
+import com.newswebsite.main.dto.RoleDTO;
 import com.newswebsite.main.dto.UserDTO;
 import com.newswebsite.main.entity.User;
+import com.newswebsite.main.mapper.CollectionMapper;
 import com.newswebsite.main.repository.UserRepo;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,16 +16,20 @@ import java.util.List;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepo userRepo;
+    private final UserRepo userRepo;
 
-    private final ModelMapper mapper = new ModelMapper();
+    private final CollectionMapper mapper;
+
+    public CustomUserDetailsService(UserRepo userRepo, CollectionMapper mapper) {
+        this.userRepo = userRepo;
+        this.mapper = mapper;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = isEmail(username) ? userRepo.findByEmail(username) : userRepo.findByUsername(username);
         if (user == null) throw new UsernameNotFoundException("Không tìm thấy người dùng với thông tin " + username);
-        List<GrantedAuthority> authorities = new ArrayList<>(user.getAuthorities());
+        List<RoleDTO> authorities = new ArrayList<>(mapper.map(user.getAuthorities(), RoleDTO.class));
         UserDTO userDTO = mapper.map(user, UserDTO.class);
         userDTO.setAuthorities(authorities);
         return userDTO;
