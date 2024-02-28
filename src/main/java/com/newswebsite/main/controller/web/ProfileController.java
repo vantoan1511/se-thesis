c769beed717f90431eb3b9fdd3c1bf9e5bdae841
vantoan1511 <vantoan1517@gmail.com>
2024-02-28 18:35:59 +0700

    Update code to load all the uploaded images and updateProfile parameter to UserDTO

--- src/main/java/com/newswebsite/main/controller/web/ProfileController.java
+++ src/main/java/com/newswebsite/main/controller/web/ProfileController.java
@@ -1,11 +1,14 @@
 package com.newswebsite.main.controller.web;
 
-import com.newswebsite.main.dto.ProfileRequest;
+import com.newswebsite.main.dto.UserDTO;
 import com.newswebsite.main.security.SecurityUtil;
+import com.newswebsite.main.service.imageservice.IImageReader;
 import com.newswebsite.main.service.userservice.IUserReader;
 import com.newswebsite.main.service.userservice.IUserWriter;
 import com.newswebsite.main.utils.FlashMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,10 +24,13 @@ public class ProfileController {
 
     private final IUserWriter userWriter;
 
+    private final IImageReader imageReader;
+
     @Autowired
-    public ProfileController(IUserReader userReader, IUserWriter userWriter) {
+    public ProfileController(IUserReader userReader, IUserWriter userWriter, IImageReader imageReader) {
         this.userReader = userReader;
         this.userWriter = userWriter;
+        this.imageReader = imageReader;
     }
 
     @GetMapping
@@ -32,17 +38,18 @@ public class ProfileController {
         String viewName = "web/profile";
         ModelAndView view = new ModelAndView(viewName);
         view.addObject("profile", userReader.getUser(username));
+        view.addObject("uploadedImages", imageReader.getFiles(username, new PageRequest(0, 99, Sort.Direction.DESC, "createdAt")));
         return view;
     }
 
     @PostMapping
     public String updateProfile(@PathVariable("username") String username,
-                                @Valid @ModelAttribute ProfileRequest profileRequest,
+                                @Valid @ModelAttribute UserDTO userDTO,
                                 RedirectAttributes attributes) {
         String loggedUsername = SecurityUtil.username();
         if (loggedUsername.equals(username)) {
             try {
-                userWriter.updateProfile(loggedUsername, profileRequest);
+                userWriter.updateProfile(userDTO);
                 attributes.addFlashAttribute("message", FlashMessage.success("Cập nhật thành công"));
             } catch (RuntimeException ex) {
                 attributes.addFlashAttribute("message", FlashMessage.danger(ex.getMessage()));
