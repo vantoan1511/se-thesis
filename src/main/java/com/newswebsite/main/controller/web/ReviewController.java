74a82a4c133f48884e5397264d76a0aa1e091580
vantoan1511 <vantoan1517@gmail.com>
2024-03-01 18:50:45 +0700

    Add handler for GET /reviews/{id}/delete

--- src/main/java/com/newswebsite/main/controller/web/ReviewController.java
+++ src/main/java/com/newswebsite/main/controller/web/ReviewController.java
@@ -1,19 +1,37 @@
 package com.newswebsite.main.controller.web;
 
+import com.newswebsite.main.service.reviewservice.IReviewWriter;
+import com.newswebsite.main.utils.FlashMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/reviews")
 public class ReviewController {
 
-    @PostMapping
-    public ModelAndView create() {
+    private final IReviewWriter reviewWriter;
 
-        String viewName = "";
-        ModelAndView view = new ModelAndView(viewName);
-        return view;
+    @Autowired
+    public ReviewController(IReviewWriter reviewWriter) {
+        this.reviewWriter = reviewWriter;
+    }
+
+    @GetMapping("/{reviewId}/delete")
+    public String deleteReview(@PathVariable("reviewId") Long id,
+                               @RequestParam(value = "redirectUrl") String redirectUrl,
+                               RedirectAttributes attributes) {
+        String viewName = "redirect:".concat(redirectUrl);
+        try {
+            reviewWriter.delete(id);
+            attributes.addFlashAttribute("message", FlashMessage.success("Đã xóa thành công!"));
+        } catch (RuntimeException ex) {
+            attributes.addFlashAttribute("message", FlashMessage.danger(ex.getMessage()));
+        }
+        return viewName;
     }
 }
