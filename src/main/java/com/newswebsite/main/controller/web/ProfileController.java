49c02d932db40475c5eba9ab893c59bafa41069d
vantoan1511 <vantoan1517@gmail.com>
2024-03-01 15:38:13 +0700

    Update getProfile to show recentReviews

--- src/main/java/com/newswebsite/main/controller/web/ProfileController.java
+++ src/main/java/com/newswebsite/main/controller/web/ProfileController.java
@@ -1,17 +1,21 @@
 package com.newswebsite.main.controller.web;
 
+import com.newswebsite.main.dto.ImageDTO;
+import com.newswebsite.main.dto.ReviewDTO;
 import com.newswebsite.main.dto.UserDTO;
 import com.newswebsite.main.security.SecurityUtil;
 import com.newswebsite.main.service.imageservice.IImageReader;
+import com.newswebsite.main.service.reviewservice.IReviewReader;
 import com.newswebsite.main.service.userservice.IUserReader;
 import com.newswebsite.main.service.userservice.IUserWriter;
 import com.newswebsite.main.utils.FlashMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
@@ -26,20 +30,26 @@ public class ProfileController {
 
     private final IImageReader imageReader;
 
+    private final IReviewReader reviewReader;
+
     @Autowired
-    public ProfileController(IUserReader userReader, IUserWriter userWriter, IImageReader imageReader) {
+    public ProfileController(IUserReader userReader, IUserWriter userWriter, IImageReader imageReader, IReviewReader reviewReader) {
         this.userReader = userReader;
         this.userWriter = userWriter;
         this.imageReader = imageReader;
+        this.reviewReader = reviewReader;
     }
 
     @GetMapping
-    public ModelAndView getProfile(@PathVariable("username") String username) {
+    public String getProfile(@PathVariable("username") String username,
+                             Model model) {
         String viewName = "web/profile";
-        ModelAndView view = new ModelAndView(viewName);
-        view.addObject("profile", userReader.getUser(username));
-        view.addObject("uploadedImages", imageReader.getFiles(username, new PageRequest(0, 99, Sort.Direction.DESC, "createdAt")));
-        return view;
+        Page<ImageDTO> uploadedImages = imageReader.getFiles(username, new PageRequest(0, 99, Sort.Direction.DESC, "createdAt"));
+        Page<ReviewDTO> recentReviews = reviewReader.getAllReviewsByUsername(username, new PageRequest(0, 4, Sort.Direction.DESC, "createdAt"));
+        model.addAttribute("uploadedImages", uploadedImages);
+        model.addAttribute("recentReviews", recentReviews);
+        model.addAttribute("profile", userReader.getUser(username));
+        return viewName;
     }
 
     @PostMapping
