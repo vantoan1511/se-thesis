1914b3b5ec992592830db81d7082119158a17a64
vantoan1511 <vantoan1517@gmail.com>
2024-03-03 19:50:58 +0700

    Update to increase traffic whenever article is visited

--- src/main/java/com/newswebsite/main/controller/web/ArticleController.java
+++ src/main/java/com/newswebsite/main/controller/web/ArticleController.java
@@ -4,6 +4,7 @@ import com.newswebsite.main.dto.ArticleDTO;
 import com.newswebsite.main.enums.Role;
 import com.newswebsite.main.security.SecurityUtil;
 import com.newswebsite.main.service.articleservice.IArticleReader;
+import com.newswebsite.main.service.articleservice.IArticleWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +21,12 @@ public class ArticleController {
 
     private final IArticleReader articleReader;
 
+    private final IArticleWriter articleWriter;
+
     @Autowired
-    public ArticleController(IArticleReader articleReader) {
+    public ArticleController(IArticleReader articleReader, IArticleWriter articleWriter) {
         this.articleReader = articleReader;
+        this.articleWriter = articleWriter;
     }
 
     @GetMapping
@@ -33,7 +37,9 @@ public class ArticleController {
 
         ArticleDTO articleDTO = (previewMode && (roles.contains(Role.WRITER.name()) || roles.contains(Role.ADMIN.name()))) ?
                 articleReader.getByAlias(alias) :
-                articleReader.getAllPublished(alias);
+                articleReader.getPublished(alias);
+
+        articleWriter.increaseTraffic(alias);
 
         ModelAndView view = new ModelAndView(viewName);
         view.addObject("article", articleDTO);
