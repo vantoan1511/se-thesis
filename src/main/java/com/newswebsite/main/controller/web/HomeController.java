f784eec340b31fe7022ccccaeefd05852b980aaf
vantoan1511 <vantoan1517@gmail.com>
2024-03-03 19:50:11 +0700

    Put popularArticles

--- src/main/java/com/newswebsite/main/controller/web/HomeController.java
+++ src/main/java/com/newswebsite/main/controller/web/HomeController.java
@@ -1,11 +1,16 @@
 package com.newswebsite.main.controller.web;
 
+import com.newswebsite.main.dto.ArticleDTO;
 import com.newswebsite.main.service.articleservice.IArticleReader;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller(value = "WebHomeController")
 @RequestMapping({"/home", "/"})
@@ -21,8 +26,11 @@ public class HomeController {
     public ModelAndView getHomePage() {
         String viewName = "web/home";
         ModelAndView view = new ModelAndView(viewName);
+        Pageable pageable = new PageRequest(0, 10, Sort.Direction.DESC, "traffic");
+        List<ArticleDTO> popularArticles = articleReader.getAllPublished(pageable).getContent();
         view.addObject("featured", articleReader.getFeaturedArticles(new PageRequest(0, 3)));
         view.addObject("latest", articleReader.getLatestArticles(new PageRequest(0, 10)));
-        return view;    
+        view.addObject("popularArticles", popularArticles);
+        return view;
     }
 }
