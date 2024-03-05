21604d7d56667bf2a5c1e2a5d4efeabc1e266206
vantoan1511 <vantoan1517@gmail.com>
2024-03-05 13:34:15 +0700

    Remove validation

--- src/main/java/com/newswebsite/main/dto/ReviewDTO.java
+++ src/main/java/com/newswebsite/main/dto/ReviewDTO.java
@@ -14,13 +14,10 @@ import javax.validation.constraints.NotNull;
 @Setter
 public class ReviewDTO extends BaseDTO {
 
-    @NotBlank(message = "Mục này là bắt buộc")
     private String text;
-    @NotNull(message = "Mục này là bắt buộc")
     private Long userId;
     private String userFirstName;
     private String userLastName;
-    @NotNull(message = "Mục này là bắt buộc")
     private Long articleId;
     private String articleAlias;
     private Long parentId;
