f9d852ba25fbc555209c3ef4eb5abec74df01134
vantoan1511 <vantoan1517@gmail.com>
2024-03-13 10:27:42 +0700

    Optimize query

--- src/main/java/com/newswebsite/main/repository/CategoryRepo.java
+++ src/main/java/com/newswebsite/main/repository/CategoryRepo.java
@@ -4,12 +4,17 @@ import com.newswebsite.main.entity.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CategoryRepo extends JpaRepository<Category, Long> {
-    Category findByAlias(String alias);
 
+    @Query("SELECT c, p FROM Category c LEFT JOIN c.parent p WHERE c.alias = :alias")
+    Category findByAlias(@Param("alias") String alias);
+
+    @Query("SELECT c FROM Category c WHERE c.parent is NULL")
     List<Category> findAll();
 
     Page<Category> findAll(Pageable pageable);
