1d48a44b98ea3adb9e859f5fa5cbe1ba950f03fd
vantoan1511 <vantoan1517@gmail.com>
2024-03-13 19:56:54 +0700

    Update to get value

--- src/main/java/com/newswebsite/main/enums/ArticleState.java
+++ src/main/java/com/newswebsite/main/enums/ArticleState.java
@@ -1,5 +1,15 @@
 package com.newswebsite.main.enums;
 
 public enum ArticleState {
-    DRAFT, PENDING, APPROVED, PUBLISHED, UNPUBLISHED, TRASH
+    DRAFT("Nháp"), PENDING("Đang chờ duyệt"), APPROVED("Đã được chấp nhận"), PUBLISHED("Đã đăng tải"), UNPUBLISHED("Đã gỡ"), TRASH("Đã xóa");
+
+    private final String value;
+
+    ArticleState(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
 }
