941974148b318df050d2e628fef10b8c3f9ecd56
vantoan1511 <vantoan1517@gmail.com>
2024-03-16 22:47:41 +0700

    Optimize query

--- src/main/java/com/newswebsite/main/repository/UserRepo.java
+++ src/main/java/com/newswebsite/main/repository/UserRepo.java
@@ -2,11 +2,14 @@ package com.newswebsite.main.repository;
 
 import com.newswebsite.main.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
-    User findByUsername(String username);
+    @Query("SELECT u FROM User u JOIN FETCH u.authorities a WHERE u.username=:username")
+    User findByUsername(@Param("username") String username);
 
     User findByToken(String token);
 }
